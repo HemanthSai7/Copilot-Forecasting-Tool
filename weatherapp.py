5f36fff7f490f11635f1bb5be4fa1d5b20522896
Vishesh <vishesht27@gmail.com>
2023-06-11 22:10:18 +0530

    Added speaking

--- weatherapp.py
+++ weatherapp.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import azure.cognitiveservices.speech as speechsdk
 import argparse
 
 from dotenv import load_dotenv
@@ -20,6 +21,20 @@ class Weather:
             'description': data['weather'][0]['description']
         }
         return weather
+
+
+def speak_weather(city, weather):
+    speech_config = speechsdk.SpeechConfig(subscription="537be803-988c-4bdf-85b6-5027b562f13c", region="centralindia")
+    synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+    
+    text = f"The current weather in {city} is {weather['description']}. The temperature is {weather['temperature']} degrees Celsius, with a humidity of {weather['humidity']} percent."
+    result = synthesizer.speak_text_async(text).get()
+    
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        audio_stream = result.audio_data_stream
+        return audio_stream
+    else:
+        return None
     
 def main():
     parser = argparse.ArgumentParser(description='Weather Application')
@@ -38,10 +53,23 @@ def main():
 
     weather = weather_app.get_weather()
 
+    audio_stream = speak_weather(args.city, weather)
+    
+
     print(f"Weather in {args.city}:")
     print(f"Temperature: {weather['temperature']}°C")
     print(f"Humidity: {weather['humidity']}%")
     print(f"Description: {weather['description']}")
+
+
+    if audio_stream:
+        audio_file = "weather_forecast.wav"
+        with open(audio_file, "wb") as file:
+            file.write(audio_stream.get_wav_data())
+
+        print("Weather forecast audio generated and saved as weather_forecast.wav.")
+    else:
+        print("Failed to generate weather forecast audio.")
 
 if __name__ == '__main__':
     main()    
