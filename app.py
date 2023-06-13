2334b7d7c703c9cf4bc082200323794b954387bd
Vishesh <vishesht27@gmail.com>
2023-06-13 17:32:41 +0530

    Documented the code

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
+# Import the necessary modules
 import requests
-
 import azure.cognitiveservices.speech as speechsdk
 
+# Define a Weather class
 class Weather:
-    def __init__(self, city, api_key,azure_sub_key):
+    # Define the __init__ method, which is called when a new instance of the class is created
+    def __init__(self, city, api_key, azure_sub_key):
+        # Store the city, API key, and Azure subscription key as instance variables
         self.city = city
         self.api_key = api_key
-        self.azure_sub_key=azure_sub_key
+        self.azure_sub_key = azure_sub_key
 
+    # Define a method to get the weather for the specified city
     def get_weather(self):
+        # Construct the URL for the OpenWeatherMap API call
         url = f'https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}&units=metric'
+        # Send a GET request to the API and store the response
         response = requests.get(url)
+        # Parse the response as JSON and extract the relevant weather data
         data = response.json()
         weather = {
             'temperature': data['main']['temp'],
             'humidity': data['main']['humidity'],
             'description': data['weather'][0]['description']
         }
+        # Return the weather data as a dictionary
         return weather
     
-    def speak_weather(self,city, weather):
+    # Define a method to speak the weather for the specified city
+    def speak_weather(self, city, weather):
+        # Create a SpeechConfig object with the Azure subscription key and region
         speech_config = speechsdk.SpeechConfig(subscription=self.azure_sub_key, region="centralindia")
+        # Create a SpeechSynthesizer object with the SpeechConfig
         synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
         
+        # Construct a string with the weather information
         text = f"The current weather in {city} is {weather['description']}. The temperature is {weather['temperature']} degrees Celsius, with a humidity of {weather['humidity']} percent."
+        # Call the SpeechSynthesizer's speak_text_async method with the weather string
         result = synthesizer.speak_text_async(text).get()
         
+        # If the speech synthesis is successful, return the audio stream
         if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
             audio_stream = result.audio_data_stream
             return audio_stream
+        # Otherwise, return None
         else:
             return None
--- weather.py
+++ weather.py
@@ -1,41 +1,54 @@
+# Import the necessary modules
 import os
 import argparse
-
 from app import Weather
-
 from dotenv import load_dotenv
+
+# Load environment variables from .env file
 load_dotenv()
 
+# Define the main function
 def main():
+    # Create an ArgumentParser object with a description of the program
     parser = argparse.ArgumentParser(description='Weather Application')
-    parser.add_argument('--city', '-c',help='City name')
-    parser.add_argument('--api-key','-KEY',help='API key for OpenWeatherMap')
-    parser.add_argument('--azure-sub-key',help='API key for Azure Speech Service')
-    parser.add_argument('--listen','-v',help='Flag to enable voice output')
-
+    # Add command-line arguments for the city name, API key, Azure subscription key, and voice output flag
+    parser.add_argument('--city', '-c', help='City name')
+    parser.add_argument('--api-key', '-KEY', help='API key for OpenWeatherMap')
+    parser.add_argument('--azure-sub-key', help='API key for Azure Speech Service')
+    parser.add_argument('--listen', '-v', help='Flag to enable voice output')
+    # Parse the command-line arguments
     args = parser.parse_args()
 
+    # Get the API key from the command-line arguments or environment variable
     api_key = args.api_key or os.getenv('API_KEY')
+    # Get the Azure subscription key from the command-line arguments or environment variable
     azure_sub_key = args.azure_sub_key or os.getenv('AZURE_SUB_KEY')
 
+    # Check if the API key is missing
     if not api_key:
         print("API key is missing. Please provide the API key as an argument or set it in the environment variable (API_KEY).")
         return
     
+    # Check if the Azure subscription key is missing
     if not azure_sub_key:
         print("Azure subscription key is missing. Please provide the key as an argument or set it in the environment variable (AZURE_SUB_KEY).")
         return
     
-    weather_app = Weather(args.city, api_key,azure_sub_key)
+    # Create a Weather object with the city name, API key, and Azure subscription key
+    weather_app = Weather(args.city, api_key, azure_sub_key)
 
+    # Get the weather data for the specified city
     weather = weather_app.get_weather()
 
+    # Print the weather data to the console
     print(f"Weather in {args.city}:")
     print(f"Temperature: {weather['temperature']}°C")
     print(f"Humidity: {weather['humidity']}%")
     print(f"Description: {weather['description']}")
 
-    if args.listen=="Yes":
+    # Check if the voice output flag is set to "Yes"
+    if args.listen == "Yes":
+        # Generate the weather forecast audio and save it to a file
         audio_stream = weather_app.speak_weather(args.city, weather)
         if audio_stream:
             audio_file = "weather_forecast.wav"
@@ -46,5 +59,6 @@ def main():
         else:
             print("Failed to generate weather forecast audio.")
 
+# Call the main function if this script is being run directly
 if __name__ == '__main__':
-    main()    
+    main()
